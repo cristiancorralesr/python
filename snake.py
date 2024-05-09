ddee9bc2077b6820709da2db690293e94a4acc37
Cristian Alberto Corrales Ramirez <1143851641@icesi.edu.co>
2024-05-09 16:37:05 -0500

    se agrega clase 1

---
+++ snake.py
@@ -0,0 +1,19 @@
+print("Hisssssss...")
+print(0o123)
+var = 21
+
+print(var)
+var = "3.8.5"
+print("Python version: ", var)
+
+mll = 1
+
+km = mll*1.61
+
+print(km)
+
+
+x =  -1
+x = float(x)
+y = 3 * (x**3) - 2* (x**2) + 3*x -1 
+print("y =", y)
